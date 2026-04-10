678451daf61d465572012508cbbec647d6526a24
Libby Brill <libby@brillfamily.net>
2026-04-09 23:23:41 -0700

    data summary for modified column, custom function with iteration

---
+++ libby/checkpoint_2.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+tuesdata <- tidytuesdayR::tt_load('2025-06-24')
+cases_month <- tuesdata$cases_month
+# cases_year <- tuesdata$cases_year
+
+calculate_2025_prop_sc <- function(input_month) {
+  cases_month |>
+    filter(year == 2024,
+           month == input_month) |>
+    summarize(unconfirmed = sum(measles_suspect + measles_clinical,
+                                na.rm = TRUE),
+              confirmed = sum(measles_epi_linked + measles_lab_confirmed,
+                              na.rm = TRUE),
+              prop = unconfirmed / confirmed) |>
+    select(prop) |>
+    pull()
+}
+
+cases_month |>
+  filter(year == 2024) |>
+  distinct(month) |>
+  mutate(prop_suspected_confirmed = map_dbl(month, calculate_2025_prop_sc),
+         month = month(month, label = TRUE))
+
+
