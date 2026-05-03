e0ffff6d1789009024d318cbbb76cf358053c029
Libby Brill <libby@brillfamily.net>
2026-05-02 18:42:05 -0700

    updated graphs + tables

--- libby/rd.R
+++ libby/rd.R
@@ -9,46 +9,41 @@ metadata |>
   mutate(region = fct_reorder(region, prop_confirmed)) |>
   ggplot(aes(x = prop_confirmed, y = region)) +
   geom_segment(aes(x = 0, xend = prop_confirmed)) +
-  geom_point(color = "#3498db") +
+  geom_point(color = "#f39c12") +
   scale_x_continuous(labels = label_percent()) +
-  labs(title = "Proportion of Total Measles Cases that are Confirmed, by Region",
-       subtitle = "",
-       x = "",
+  labs(title = "Proportion of Confirmed Measles Cases by Region",
+       x = "Percent of Total Measles Cases that are Confirmed (%)",
        y = "",
-       caption = "") +
-  theme_bw()
+       caption = md("**Figure X** The proportion of lab confirmed and epidemiologically linked measles cases to total cases, calculated across world regions"),
+       alt = "A lollipop plot displaying the percent of total meales cases that are confirmed by region. Americas has the highest percentage with nearly all total measles cases confirmed. Eastern Mediterranean and South East Asia have the lowest percentages at just below 50%.") +
+  theme_bw() +
+  theme(plot.caption = element_textbox_simple())
 
-# table for RQ2 - table of GDP to vaccination rate for region
-calculate_region_info <- function(input_region) {
-  if (!is.character(input_region) & !is.factor(input_region)) {
-    stop("Region should be a string")
-  }
-  if (!(input_region %in% unique(metadata$region))) {
-    stop("Region not listed")
-  }
-  
-  metadata |>
-    filter(region == input_region) |>
-    summarize(mean_mcv2 = mean(mcv2, na.rm = TRUE)) |>
-    pull()
-}
+# **Figure X** shows that the Americas with a near 100% confirmation rate. This suggests a very strong diagnostic infrastructure where almost every suspected case is officially verified. The Eastern Mediterranean and South East Asia regions show the lowest proportions, with both falling just under the 50% mark. This means that more than half of the measles cases in these regions remain unconfirmed by lab or epidemiological links
 
 metadata |>
-  distinct(region) |>
   filter_out(is.na(region)) |>
-  mutate(mean_mcv2 = map_dbl(region, calculate_region_info)) |>
-  arrange(mean_mcv2) |>
+  group_by(region) |>
+  summarize(mean_GDP = mean(GDP_per_capita, na.rm = TRUE),
+            mean_mcv2 = mean(mcv2, na.rm = TRUE)) |>
+  arrange(mean_GDP) |>
   gt() |>
+  data_color(columns = mean_GDP, method = "numeric",
+             fn = col_numeric(palette = c("#ecf0f1", "#3498db"),
+                              domain = NULL)) |>
   data_color(columns = mean_mcv2, method = "numeric",
              fn = col_numeric(palette = c("#ecf0f1", "#29c1a3"),
-                                  domain = NULL)) |>
-  fmt_number(columns = mean_mcv2, decimals = 2) |>
+                              domain = NULL)) |>
+  fmt_currency(columns = mean_GDP, currency = "USD") |>
+  fmt_percent(columns = mean_mcv2, decimals = 1) |>
   cols_align(align = "left", columns = region) |>
   tab_style(style = cell_text(weight = "bold"),
             locations = cells_column_labels()) |>
-  tab_header(title = md("Ratios of GDP per Capita to MCV2 Vaccine Rate")) |>
+  tab_header(title = md("Average GDP per Capita and MCV2 Vaccine Rate by Region")) |>
   tab_caption(caption = md("**Table X** ")) |>
-  cols_label(region = "Region", mean_mcv2 = "GDP/MCV2 Proportion")
+  cols_label(region = "Region", mean_GDP = "Mean GDP (USD)", mean_mcv2 = "Mean MCV2 Rate")
+
+# **Table X** shows that a higher average region GDP is generally associated with a higher measles vaccination rate (with the exception of South East Asia, which has the second-lowest mean GDP but the second-highest MCV2 rate). Africa has the lowest GDP and the lowest MCV2, while Europe has the highest in both.
 
 # graph for RQ2 - map
 world <- ne_countries(scale = "medium", returnclass = "sf")
@@ -69,7 +64,7 @@ world |>
        x = "",
        y = "",
        fill = "Measles Vaccination Rate",
-       caption = md("**Figure 3** A map of the countries in the world colored by 2024 vaccination rates, where lighter red represents a higher vaccination rate and darker red represents a lower vaccination rate. Contries that are grayed out have unknown vaccination rates."),
+       caption = md("**Figure X** A map of the countries in the world colored by 2024 vaccination rates, where lighter red represents a higher vaccination rate and darker red represents a lower vaccination rate. Contries that are grayed out have unknown vaccination rates."),
        alt = "A world map displaying 2024 measles vaccination rates on a red color scale. Many countries in North America, Europe, and parts of Asia appear lighter, suggesting higher vaccination coverage, while several countries in central and sub-Saharan Africa are darker, suggesting lower coverage. Some countries are gray, representing unknown vaccination rates.") +
   theme_bw() +
   theme(plot.caption = element_textbox_simple(),
@@ -77,9 +72,13 @@ world |>
         axis.ticks = element_blank(),
         panel.grid = element_blank())
 
-"#29c1a3"
-"#ecf0f1"
-"#3498db"
-"#f39c12"
-"#e74c3c"
-"#859394"
+# **Figure X** shows that many countries in North America, Europe, and parts of Asia have higher vaccination coverage, while several countries in central and sub-Saharan Africa have lower coverage.
+# ADD COMPARISON TO TABLE, TALK ABOUT WHAT THE GDP IS FOR THESE REGIONS? OR MAYBE THE MEASLES RATE
+
+# Colors
+  # "#29c1a3"
+  # "#ecf0f1"
+  # "#3498db"
+  # "#f39c12"
+  # "#e74c3c"
+  # "#859394"
